b1cba80b363b433baf645b6655fdf20fc0d02d41
Cryptomnesia <82593272+CryptomnesiaWasTaken@users.noreply.github.com>
2022-09-02 22:41:45 +0200

    Update getSentence.js

--- api/getSentence.js
+++ api/getSentence.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const getSentence = async () => {
-  const jokeData = await axios.get("https://api.quotable.io/random");
+  const jokeData = await axios.get("https://api.quotable.io/random?minLength=174");
 
   return jokeData.data.content.split(" ");
 };
